168b1c5286fb76586d604335e71b8f4f6207b997
Vector BCO <vector.bco@gmail.com>
2020-05-24 04:01:38 +0200

    modified questionsxml and added sceleton for selfVerification script

---
+++ SelfVerification.ps1
@@ -0,0 +1,70 @@
+$ErrorActionPreference = 'Stop'
+#$XMLPath = "D:\Git\PowerShell.Learning\Questions.xml"
+
+function Get-Answer {
+    param (
+        [string]$question,
+        [string[]]$answers,
+        [int]$ca
+    )
+    $answerFound = $false
+    do {
+        Clear-Host
+        $questionNumber = 0
+        Write-Host "$question"
+        foreach ($answer in $answers){
+            $questionNumber++
+            Write-Host "[$questionNumber] $($answer -split '\\n' -replace '\\t',"`t" )"
+        }
+        $answerNumber = Read-Host -Prompt "Select answer 1 - $questionNumber"
+        if ($answerNumber -in @(1..$questionNumber)) {
+            $answerFound = $true
+        } else {
+            Write-Host "Provided incorrect answer. Please try again" -foreground Yellow
+            Start-Sleep -s 2
+        }
+    } until ( $answerFound )
+    if ($answerFound){
+        if (! [string]::IsNullOrEmpty($ca)){
+            [psobject]@{
+                isAnswerCorrect = $answerNumber -eq $($ca + 1);
+                answer = $answers[$answerNumber - 1]
+            }
+        } else{
+            $answers[$answerNumber - 1]
+        }
+    }
+}
+
+try{
+    $XMLPath = "$PSScriptRoot\Questions.xml"
+    if (!(Test-Path $XMLPath)){
+        $TmpPath = New-TemporaryFile 
+        $url = 'https://raw.githubusercontent.com/Vector-BCO/PowerShell.Learning/master/Questions.xml'
+        Start-BitsTransfer -Source $url -Destination $TmpPath
+        $XMLPath = $TmpPath
+    } 
+
+    if (Test-Path $XMLPath){
+        $QuestionsXML = $([xml](Get-Content $XMLPath)).Questions.question
+        $languages = $QuestionsXML | Select-Object -ExpandProperty lang -Unique
+        $SelectedLanguage = $languages | Select-Object -First 1
+        if (($languages | Measure-Object).count -gt 1){
+            Get-Answer -Qusetion "Select language from the list" -Answers $languages
+        }
+        $Questions = $QuestionsXML | Where-Object {$_.lang -eq $SelectedLanguage}
+        foreach ($Question in $Questions){
+            $ReceivedAnswer = Get-Answer -question $Question.text.'#cdata-section' -answers $Question.Answers.answer -ca $Question.ca
+            if ($ReceivedAnswer.isAnswerCorrect){
+                Write-Host "Correct answer" -ForegroundColor Green
+            } else {
+                Write-Host "Incorrect answer" -ForegroundColor Red
+            }
+            Start-Sleep -s 2
+        }
+    } else {
+        # ! ToDo add some stub
+    }
+} Catch {
+    # TODO add error handling
+}
