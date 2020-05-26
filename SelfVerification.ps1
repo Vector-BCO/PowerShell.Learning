e1296b5d1894bfcde40875e6946460a35ffa1e45
Vector BCO <vector.bco@gmail.com>
2020-05-26 03:15:09 +0200

    fixes from comments

--- SelfVerification.ps1
+++ SelfVerification.ps1
@@ -1,16 +1,28 @@
 $ErrorActionPreference = 'Stop'
 
+Function Get-MDHash {
+    param (
+        [string]$Question,
+        [string]$Answer
+    )
+    $string = $Question + $Answer
+    $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+    [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($string)))
+}
+
 function Get-Answer {
     param (
         [string]$question,
         [string[]]$answers,
-        [int]$ca
+        [string]$correctAnswer
     )
     $answerFound = $false
     do {
         Clear-Host
         $questionNumber = 0
         Write-Host "$question"
+        Write-Host '-----------------------------'
         foreach ($answer in $answers){
             $questionNumber++
             Write-Host "[$questionNumber] $($answer -split '\n' | Where-Object {$_ -notmatch '^\s*$'} | Out-String)"
@@ -24,9 +36,9 @@ function Get-Answer {
         }
     } until ( $answerFound )
     if ($answerFound){
-        if (! [string]::IsNullOrEmpty($ca)){
+        if (! [string]::IsNullOrEmpty($correctAnswer)){
             [psobject]@{
-                isAnswerCorrect = $answerNumber -eq $($ca + 1);
+                isAnswerCorrect = $correctAnswer -eq $(Get-MDHash -Question $question -Answer $answers[$answerNumber - 1]);
                 question = $question;
                 answer = $answers[$answerNumber - 1]
             }
@@ -37,37 +49,42 @@ function Get-Answer {
 }
 
 try{
-    $XMLPath = "$PSScriptRoot\Questions.xml"
-    if (!(Test-Path $XMLPath)){
+    $QuestionPath = "$PSScriptRoot\questions.json"
+    if (!(Test-Path $QuestionPath)){
         $TmpPath = New-TemporaryFile 
-        $url = 'https://raw.githubusercontent.com/Vector-BCO/PowerShell.Learning/master/Questions.xml'
+        $url = 'https://raw.githubusercontent.com/Vector-BCO/PowerShell.Learning/master/questions.json'
         Start-BitsTransfer -Source $url -Destination $TmpPath
-        $XMLPath = $TmpPath
+        $QuestionPath = $TmpPath
     } 
 
-    if (Test-Path $XMLPath){
-        $QuestionsXML = $([xml](Get-Content $XMLPath -Encoding UTF8)).Questions.question
-        $languages = $QuestionsXML | Select-Object -ExpandProperty lang -Unique
+    if (Test-Path $QuestionPath){
+        $QuestionsWithLanguages = (Get-Content $QuestionPath -Encoding UTF8 | ConvertFrom-Json).Languages
+        $languages = $QuestionsWithLanguages | Select-Object -ExpandProperty Language -Unique
         $SelectedLanguage = $languages | Select-Object -First 1
         if (($languages | Measure-Object).count -gt 1){
             Get-Answer -Qusetion "Select language from the list" -Answers $languages
         }
-        $Questions = $QuestionsXML | Where-Object {$_.lang -eq $SelectedLanguage}
+        $QuestionsWithLanguages = $QuestionsWithLanguages | Where-Object {$_.Language -eq $SelectedLanguage} | Select-Object -ExpandProperty questionCategories
         $answers = @()
-        foreach ($Question in $Questions){
-            $ReceivedAnswer = Get-Answer -question $Question.text.'#cdata-section' -answers $Question.Answers.answer -ca $Question.ca
-            if ($ReceivedAnswer.isAnswerCorrect){
-                Write-Host "Correct answer" -ForegroundColor Green
-            } else {
-                Write-Host "Incorrect answer" -ForegroundColor Red
+        foreach ($Category in $QuestionsWithLanguages){
+            $CategoryName = $Category.category
+            Write-Host "Category started: '$CategoryName'"
+            foreach ($question in $Category.questions){
+                $ReceivedAnswer = Get-Answer -question $Question.question -answers $Question.answers -correctAnswer $Question.correctAnswer
+                if ($ReceivedAnswer.isAnswerCorrect){
+                    Write-Host "Correct answer" -ForegroundColor Green
+                } else {
+                    Write-Host "Incorrect answer" -ForegroundColor Red
+                }
+                $answers += $ReceivedAnswer
+                Start-Sleep -s 2
             }
-            $answers += $ReceivedAnswer
-            Start-Sleep -s 2
         }
 
-        $answers = $answers | Foreach-Object {$_ | Select-Object @{n='question'; e={$_.question}}, @{n='answer'; e={$_.answer}}, @{n='IsAnswerCorrect'; e={$_.isAnswerCorrect}}}
-        $correctAnswer = ($($answers | Where-Object {$_.IsAnswerCorrect -eq $true} | Measure-Object).count * 100) / $($answers | Measure-Object).count
-        Write-Host "Correct answers: $correctAnswer%"
+        $answers = $answers | Foreach-Object {$_ | Select-Object @{n='question'; e={$_.question}}, @{n='answer'; e={$_.answer}}, @{n='IsAnswerCorrect'; e={$_.isAnswerCorrect}}}#, @{n='hash'; e={$_.hash}}}
+        $correctAnswerPercentage = ($($answers | Where-Object {$_.IsAnswerCorrect -eq $true} | Measure-Object).count * 100) / $($answers | Measure-Object).count
+        $correctAnswerPercentage = [math]::Round($correctAnswerPercentage, 2)
+        Write-Host "Correct answers: $correctAnswerPercentage%"
         $answers | Out-GridView
     } else {
         Throw "Questions.xml not be found, place questions.xml from GitHub to the same folder as this script is."
