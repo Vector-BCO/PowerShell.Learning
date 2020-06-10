f3071e09a74337e61f1d5021e08d34a4afb49e34
Vector BCO <vector.bco@gmail.com>
2020-06-10 15:52:56 +0200

    minor changes in optimization sample

--- optimization/sample.ps1
+++ optimization/sample.ps1
@@ -1,6 +1,6 @@
 # Пример №1 - 1 sec 712 msec
-Write-Host "[$(Get-Date)] Минимум операций - считаем от 0 до 1000000"
 Measure-Command {
+    Write-Host "[$(Get-Date)] Минимум операций - считаем от 0 до 1000000" -ForegroundColor Magenta
     $iteration = 0
     $NeedToStop = $false
     do {
@@ -11,8 +11,8 @@ Measure-Command {
 
 
 # Пример №2 - 11 min 21 sec 590 msec
-Write-Host "[$(Get-Date)] Cчитаем от 0 до 1000000 и выводим значение `$iteration в консоль"
 Measure-Command {
+    Write-Host "[$(Get-Date)] Cчитаем от 0 до 1000000 и выводим значение `$iteration в консоль" -ForegroundColor Magenta
     $iteration = 0
     $NeedToStop = $false
     do {
@@ -23,9 +23,9 @@ Measure-Command {
 }
 
 
-# Пример №3 - XX min XX sec XX msec
-Write-Host "[$(Get-Date)] Cчитаем от 0 до 1000000 и дозаписываем результат каждой операции в файл"
+# Пример №3 - ~6 hours
 Measure-Command {
+    Write-Host "[$(Get-Date)] Cчитаем от 0 до 1000000 и дозаписываем результат каждой операции в файл" -ForegroundColor Magenta
     $iteration = 0
     $NeedToStop = $false
     $TestFile1 = New-TemporaryFile #[2]
@@ -38,9 +38,9 @@ Measure-Command {
 }
 
 
-# Пример №4 - XX min XX sec XX msec
-Write-Host "[$(Get-Date)] Значения счетчика считывается из файла, к нему добавляется 1 после чего оно записывается в файл (1м раз)"
+# Пример №4 - > 80 hours
 Measure-Command {
+    Write-Host "[$(Get-Date)] Значения счетчика считывается из файла, к нему добавляется 1 после чего оно записывается в файл (1м раз)"  -ForegroundColor Magenta
     $iteration = 0
     $NeedToStop = $false
     $TestFile2 = New-TemporaryFile #[3]
@@ -48,8 +48,8 @@ Measure-Command {
     Write-Host "[DBG] Outputfile path '$TestFile2'"
     do {
         if( $iteration -ge 1000000 ){ $NeedToStop = $true }
-        $iteration = [int](Get-Content $TestFile2) #[3]
+        $iteration = [int]((Get-Content $TestFile2)[-1] -replace '\D') #[3]
         $iteration++
-        $iteration | Out-File $TestFile2 #[3]
+        $iteration | Out-File $TestFile2 -Append #[3]
     } until ($NeedToStop)
 }
